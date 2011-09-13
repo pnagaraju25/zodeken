5543b3afd222efae93ec27b7a177f53d2ff464d0
me@ndthuan.com <me@ndthuan.com@03ff2d8a-2003-d8e0-cec7-7ca2a9846ff0>
2011-09-13 18:49:30 +0000

    Rebuilt the release-latest tag (1.1)

---
+++ tags/release-latest/Zodeken/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @package Zodeken
+ * @author Thuan Nguyen <me@ndthuan.com>
+ * @copyright Copyright(c) 2011 Thuan Nguyen <me@ndthuan.com>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @version $Id$
+ */
+class Zodeken_Exception extends Exception {}
---
+++ tags/release-latest/Zodeken/ZfTool/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'Zodeken/Exception.php';
+
+/**
+ * @package Zodeken
+ * @author Thuan Nguyen <me@ndthuan.com>
+ * @copyright Copyright(c) 2011 Thuan Nguyen <me@ndthuan.com>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @version $Id$
+ */
+class Zodeken_ZfTool_Exception extends Zodeken_Exception {}
