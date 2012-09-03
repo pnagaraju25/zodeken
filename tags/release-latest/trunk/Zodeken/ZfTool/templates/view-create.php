dbd919ef88bc4200275400a2b693b3d8933ba04e
me@ndthuan.com <me@ndthuan.com@03ff2d8a-2003-d8e0-cec7-7ca2a9846ff0>
2012-09-03 05:36:57 +0000

    Delete release-latest tag

--- tags/release-latest/trunk/Zodeken/ZfTool/templates/view-create.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return <<<CODE
-<h1>Create $tableDefinition[baseClassName]</h1>
-
-<?php
-echo \$this->form;
-CODE;
-?>
--- tags/release-latest/trunk/Zodeken/ZfTool/templates/view-update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return <<<CODE
-<h1>Update $tableDefinition[baseClassName]: <?php echo \$this->row->getZodekenAutoLabel(); ?></h1>
-
-<?php
-echo \$this->form;
-CODE;
-?>
