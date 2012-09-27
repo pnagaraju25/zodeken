918423efba0a320354b646f6ab779a4157cb0906
me@ndthuan.com <me@ndthuan.com@03ff2d8a-2003-d8e0-cec7-7ca2a9846ff0>
2012-09-27 09:00:13 +0000

    Add 'dest' to edit/delete links in view-index and controller

--- trunk/Zodeken/ZfTool/templates/default/view-update.php
+++ trunk/Zodeken/ZfTool/templates/default/view-update.php
@@ -4,6 +4,6 @@ return <<<CODE
 <h1>Update $tableDefinition[baseClassName]: <?php echo \$this->row->getZodekenAutoLabel(); ?></h1>
 
 <?php
-echo \$this->form->setAction(\$this->url());
+echo \$this->form->setAction(\$_SERVER['REQUEST_URI']);
 CODE;
 ?>
